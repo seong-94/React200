950fd50ea197d4eeaa66e66a4d542fb26840f1ca
44502699 <68951572+seong-94@users.noreply.github.com>
2022-07-28 10:51:21 +0900

    입문003
    Import component

--- react200/src/App.js
+++ react200/src/App.js
@@ -1,3 +1,4 @@
+import ImportComponent from "./Components/ImportComponents";
 import "./App.css";
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <div className="App">
       <h1>React 200 Start!</h1>
       <p>HTML 적용하기 </p>
+      <ImportComponent />
     </div>
   );
 }
---
+++ react200/src/Components/ImportComponents.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+function ImportComponent() {
+  return <h2>This is Imported Component</h2>;
+}
+
+export default ImportComponent;
